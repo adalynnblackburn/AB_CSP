9a62e46e927e301b333ada043ac22c6d8ddd7223
adalynnblackburn <adalynn.blackburn@ucas-edu.net>
2025-08-29 14:42:35 -0600

    variable notes

---
+++ practices/variables_practice.py
@@ -0,0 +1,17 @@
+#What is one way to have your program give an output?
+print("Welcome to the Variables notes!")
+
+#Examples of Variables
+school = "ucas"
+single_digit = 4
+how_much = 10
+
+print("how_much")
+
+how_much = 99999999999999999999999999
+
+print("how_much")
+
+name = input("What is your name?")
+
+print("Hello", name)
