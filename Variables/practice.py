87a111ffcc31637d41c0b7b6b63f8624ffbd5673
adalynnblackburn <adalynn.blackburn@ucas-edu.net>
2025-09-02 14:40:24 -0600

    First_Program.py

---
+++ Variables/practice.py
@@ -0,0 +1,45 @@
+#AB_7
+
+name = "Adalynn"
+
+single_digit = 2
+
+tripple_digit = 114
+
+breakfast = "nothing"
+
+favorite_color = "green"
+
+school_name = "UCAS"
+
+Year = 2025
+
+eye_color = "blue"
+
+age = 14
+
+favorite_subject = "Biology"
+
+print(name)
+
+print(single_digit)
+
+print(tripple_digit)
+
+print(breakfast)
+
+print(favorite_color)
+
+print(school_name)
+
+print(Year)
+
+print(eye_color)
+
+print(age)
+
+print(favorite_subject)
+
+birthday = input("When is your birthday?")
+
+print(birthday, "is a great birthday")
