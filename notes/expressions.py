46e3e8517405230cea905a3b822280fdcbc62dee
adalynnblackburn <adalynn.blackburn@ucas-edu.net>
2025-09-04 14:41:15 -0600

    expression_notes

---
+++ notes/expressions.py
@@ -0,0 +1,30 @@
+# AB 7th Expressions notes
+
+# What is an algorithm?
+name = input("What is your name:\n")
+print("Hello",name)
+
+# Treyson has 12 apples, he has 5 friends that he wants to give apples to. How many apples does each friend get?
+apples = 12
+friends = 5
+print("Each friend gets", apples/friends, "apples!")
+
+# Average age of a group of 4 people
+friend1 = 13
+friend2 = 2
+friend3 = 65
+friend4 = 34
+total = friend1+friend2+friend3+friend4/4
+print("The average age of the 4 friends is", total)
+
+num_one = 12
+num_two = 7
+
+
+print("Addition(+): ", num_one+num_two)
+print("Subtraction(-): ", num_one-num_two)
+print("Multiplication(*): ", num_one*num_two)
+print("Division(/): ", num_one/num_two)
+print("Exponents(**): ", num_one**num_two)
+print("Integer Division(//): ", num_one//num_two)
+print("Modulo(%): ", num_one%num_two)
