3b687d6b0672af1f5f689decf2c14267c63d27c3
adalynnblackburn <adalynn.blackburn@ucas-edu.net>
2025-09-05 14:42:35 -0600

    stuff (expression/budget stuff)

--- notes/expressions.py
+++ notes/expressions.py
@@ -17,14 +17,19 @@ friend4 = 34
 total = friend1+friend2+friend3+friend4/4
 print("The average age of the 4 friends is", total)
 
-num_one = 12
-num_two = 7
-
-
-print("Addition(+): ", num_one+num_two)
-print("Subtraction(-): ", num_one-num_two)
-print("Multiplication(*): ", num_one*num_two)
-print("Division(/): ", num_one/num_two)
-print("Exponents(**): ", num_one**num_two)
-print("Integer Division(//): ", num_one//num_two)
-print("Modulo(%): ", num_one%num_two)
+num_one = int(input("Tell me a number:\n"))
+num_two = float(input("Tell me another number\n"))
+num_one += num_two
+print("Addition(+): ", num_one)
+num_one -= num_two
+print("Subtraction(-): ", num_one)
+num_one *= num_two
+print("Multiplication(*): ", num_one)
+num_one /= num_two
+print("Division(/): ", round(num_one, 2)) # round(number to round, number of decimal places)
+num_one **= num_two
+print("Exponents(**): ", num_one)
+num_one //= num_two
+print("Integer Division(//): ", num_one)
+num_one %= num_two
+print("Modulo(%): ", num_one)
---
+++ practices/budget_calculator.py
@@ -0,0 +1,10 @@
+# AB 7th Budget Calculator
+
+income = 1
+rent = 1
+utilities = 1
+groceries = 1
+transportation = 1
+savings = income*.1
+spend = income-(utilities+rent+groceries+transportation+savings)
+percent_rent = rent/income*100
