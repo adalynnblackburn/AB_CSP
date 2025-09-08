527b0602651d0b93d4a03f37e005cb2e3c430a8e
adalynnblackburn <adalynn.blackburn@ucas-edu.net>
2025-09-08 14:41:44 -0600

    started_string_notes

--- notes/string.py
+++ notes/string.py
@@ -1,3 +1,15 @@
-# AB String Notes 7th
+# AB 7th String Notes
 
 print("I did it!")
+
+#Examples
+first_name = input("What is your first name: \n").strip().title()
+
+last_name = input("What is your last name: \n").strip().title()
+
+full_name = first_name + " " + last_name
+
+sentence = 'The quick brown fox jumps over the lazy dog.'
+print(sentence.strip())
+
+print("Welcome to my program", full_name + "!")
