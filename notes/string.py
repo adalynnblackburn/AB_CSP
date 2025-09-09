128f182a0a4144da3a2b68c0c437bbc4501f84aa
adalynnblackburn <adalynn.blackburn@ucas-edu.net>
2025-09-09 14:40:58 -0600

    finished_string_notes

--- notes/string.py
+++ notes/string.py
@@ -9,11 +9,22 @@ last_name = input("What is your last name: \n").strip().title()
 
 full_name = first_name + " " + last_name
 
-sentence = 'The quick brown fox jumps over the lazy dog.'
-print(sentence.strip())
-
+sentence = 'The quick brown fox jumps over the lazy dog.'.strip()
+print(sentence.find("jumps"))
+print(sentence[20:25])
+print(sentence[sentence.find("lazy"): len("lazy")+sentence.find("lazy")])
+print(len(first_name))
 print("Welcome to my program", full_name + "!")
 
 # Sanitazation and Stupid Proofing
 
 # Debugging is fixing code!
+    #Syntax Error
+error = "This is a bug"
+    #Logic Error
+numOne = 1
+numTwo = 2
+print(numOne+numTwo)
+    #Run-Time Error
+num = 3
+print("My favorite number is", num)
