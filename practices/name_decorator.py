777471c68f85196be19b024cd9379a684f5cd458
adalynnblackburn <adalynn.blackburn@ucas-edu.net>
2025-09-10 14:28:42 -0600

    made_name_decorator

---
+++ practices/name_decorator.py
@@ -0,0 +1,9 @@
+# AB 7th Name Decorator
+
+name = input("What is your first name: \n").strip().title()
+last_name = input("What is your last name: \n").strip().title()
+
+full_name = name + " " +  last_name 
+
+print(":)---(:" + full_name + ":)---(:")
+print("Great!")
