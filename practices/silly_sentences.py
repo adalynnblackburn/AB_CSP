9cdfded3ddbed20181d883678d3026dbbcee240a
adalynnblackburn <adalynn.blackburn@ucas-edu.net>
2025-09-10 14:42:10 -0600

    started_silly_sentences

---
+++ practices/silly_sentences.py
@@ -0,0 +1,8 @@
+# AB 7th Silly Sentences
+
+word_1 = input("Give me any color: \n").strip().lower()
+word_2 = input("Now give me any number: \n").strip().lower()
+word_3 = input("Now think of a vehicle: \n").strip().lower()
+word_4 = input("Please give me a family member: \n").strip().lower()
+word_5 = input("And another family member: \n").strip().lower()
+word_6 = input("Now give me any item: \n").strip().lower()
