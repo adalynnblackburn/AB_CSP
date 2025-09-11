e653dd34e687d6974418350c5b2eabfcd1500048
programming <adalynn.blackburn@ucas-edu.net>
2025-09-11 03:32:46 +0000

    finished_silly_sentences

--- practices/silly_sentences.py
+++ practices/silly_sentences.py
@@ -1,8 +1,10 @@
 # AB 7th Silly Sentences
 
-word_1 = input("Give me any color: \n").strip().lower()
+word_1 = input("Give me any color: \n").strip().title()
 word_2 = input("Now give me any number: \n").strip().lower()
 word_3 = input("Now think of a vehicle: \n").strip().lower()
-word_4 = input("Please give me a family member: \n").strip().lower()
-word_5 = input("And another family member: \n").strip().lower()
-word_6 = input("Now give me any item: \n").strip().lower()
+word_4 = input("Please give me a name: \n").strip().title()
+word_5 = input("And another name: \n").strip().title()
+word_6 = input("Now give me any plural item: \n").strip().lower()
+
+print("My family is taking a trip to " + word_1 + " beach, " + word_2 + " hours away. We are riding in a " + word_3 + " with " + word_4 + " and " + word_5 + ". I can't wait to collect " + word_6 + " at the beach. ")
