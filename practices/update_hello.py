93bca8eddbe9e375f9c3df34de626984a588f940
adalynnblackburn <adalynn.blackburn@ucas-edu.net>
2025-09-16 14:33:02 -0600

    finished hello world update

---
+++ practices/update_hello.py
@@ -0,0 +1,11 @@
+# AB 7th Update Hello World
+
+def hello(name):
+    print(f"Hello, {name}!")
+
+name = input("What is your name?\n")
+hello("Andrew")
+hello("Tia")
+hello("Gary")
+hello("Addy")
+hello("Tim")
