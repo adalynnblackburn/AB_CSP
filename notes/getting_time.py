e1ad20c162dfa2328645f7524e61629a490cd9b9
adalynnblackburn <adalynn.blackburn@ucas-edu.net>
2025-09-19 14:03:31 -0600

    Time Notes

---
+++ notes/getting_time.py
@@ -0,0 +1,16 @@
+# AB 7th Notes about getting the time
+
+import time
+import datetime
+
+epoch = time.time()
+readable_time = time.ctime(epoch)
+
+print(f"The time in seconds since Jan 1, 1970: {epoch}")
+print(f"The time is: {readable_time}")
+
+now = datetime.datetime.now()
+hour = now.hour
+
+print(f"The time is: {now}")
+print(f"The hour is: {hour}")
