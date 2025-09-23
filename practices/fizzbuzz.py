b63ab0c52e42a2bfb6d1e9cc79daf56db4a1347a
adalynnblackburn <adalynn.blackburn@ucas-edu.net>
2025-09-23 14:23:59 -0600

    re-recorrected FizzBuzz FINALLY!!!

--- practices/fizzbuzz.py
+++ practices/fizzbuzz.py
@@ -2,12 +2,12 @@
 
 x = 1
 while x < 51:
-    if x % 3 == 0:
-        print("Fizz")
-    elif x % 3 and x % 5 == 0:
+    if x % 15 == 0:
         print("FizzBuzz")
     elif x % 5 == 0:
         print("Buzz")
+    elif x % 3 == 0:
+        print("Fizz")
     else:
         print(f"{x}")
     x += 1
