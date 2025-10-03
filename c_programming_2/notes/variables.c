4f40140856af46b9db21b22edd970db8c907c7dc
adalynnblackburn <adalynn.blackburn@ucas-edu.net>
2025-10-03 14:42:19 -0600

    finished variables notes

--- c_programming_2/notes/variables.c
+++ c_programming_2/notes/variables.c
@@ -2,13 +2,21 @@
 #include <stdio.h>
 
 int main(void){
-    int grade = 95; //4 bytes
-    float pi = 3.14; //4bytes
+    int grade; //4 bytes
+    const float pi = 3.14; //4bytes
     double long_pi = 3.1415926358; //8 bytes
-    char letter_grade = 'A'; //1 byte
+    char letter_grade; //1 byte
     char name[] = "Adalynn"; //list of characters
-    printf("%s did it!", name); //%s is place holder for variable (s inserts string)(d inserts integer)(c inserts a single character)
-    printf("You have a %d, in the class. That is an %c", grade, letter_grade);
+//User input
+    printf("What is your grade percentage as a whole number: ");
+    scanf("%d", &grade);
+
+    printf("What is your letter grade: ");
+    scanf(" %c", letter_grade);
+
+
+    printf("%s did it!\n", name); //%s is place holder for variable (s inserts string)(d inserts integer)(c inserts a single character)
+    printf("You have a %d, in the class. That is an %c\n", grade, letter_grade);
 
     return 0;
 }
