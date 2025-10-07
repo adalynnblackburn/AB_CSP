ed4daaf757d5c1e13d086b05cadcc8894311eda5
programming <adalynn.blackburn@ucas-edu.net>
2025-10-07 03:30:40 +0000

    finished first program c

--- c_programming_2/practices/first_program.c
+++ c_programming_2/practices/first_program.c
@@ -3,6 +3,10 @@
 #include <stdio.h>
 
 int main(void){
+    char name[50];
+    printf("What is your name human?");
+    scanf("%49s", name);
+    printf("Hello, %s :)\n", name);
     
     return 0;
 }
