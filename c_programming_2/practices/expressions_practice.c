0eb6e7433c9ce85126f17839af40aaf509517cef
programming <adalynn.blackburn@ucas-edu.net>
2025-10-09 01:27:32 +0000

    finished whatever the heck

--- c_programming_2/practices/expressions_practice.c
+++ c_programming_2/practices/expressions_practice.c
@@ -3,16 +3,42 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void){
-    printf("%d\n", 7-24/8*4+6);
-    printf("%d\n", 18/3-7+2*5);
-    printf("%d\n", 6*4/12+72/8-9);
-    printf("%d\n", (17-6/2)+4*3);
-    printf("%d\n", -2*(1*4-2/2)+(6+2-3));
-    printf("%d\n", -1*((3-4*7)/5)-2*24/6);
-    printf("%.0f\n", (3*pow(5,2)/15)-(5-pow(2,2)));
-    printf("%.0f\n", pow(1,4)*pow(2,2)+pow(3,3))-pow(2,5)/4;
-    printf("%.0f\n", (22/2-2*5)**2+(4-6/6)**2);
+int main(){
+    int result_1;
+    result_1 = 7-24/8*4+6;
+    printf("The result of the equation, 7-24/8*4+6, is %d\n", result_1);
+
+    int result_2;
+    result_2 = 18/3-7+2*5;
+    printf("The result of the equation, 18/3-7+2*5, is %d\n", result_2);
+
+    int result_3;
+    result_3 = 6*4/12+72/8-9;
+    printf("The result of the equation, 6*4/12+72/8-9, is %d\n", result_3);
+
+    int result_4;
+    result_4 = (17-6/2)+4*3;
+    printf("The result of the equation, (17-6/2)+4*3, is %d\n", result_4);
+
+    int result_5;
+    result_5 = -2*(1*4-2/2)+(6+2-3);
+    printf("The result of the equation, -2*(1*4-2/2)+(6+2-3), is %d\n", result_5);
+
+    int result_6;
+    result_6 = -1*((3-4*7)/5)-2*24/6;
+    printf("The result of the equation, -1*((3-4*7)/5)-2*24/6, is %d\n", result_6);
+
+    int result_7;
+    result_7 = ((int) (3*pow(5,2)/15)-(5-pow(2,2)));
+    printf("The result of the equation, (3*pow(5,5)/15)-(5-pow(2,2)), is %d\n", result_7);
+
+    int result_8;
+    result_8 = ((int) (pow(1,4)*pow(2,2)+pow(3,3)-pow(2,5)/4));
+    printf("The result of the equation, (pow(1,4)*pow(2,2)+pow(3,3)-pow(2,5)/4), is %d\n", result_8);
+
+    int result_9;
+    result_9 = ((int) pow((22/2-2*5),2)+pow((4-6/6),2));
+    printf("The result of the equation, pow((22/2-2*5),2)+pow((4-6/6),2), is %d\n", result_9);
 
     return 0;
 
