cd5e0a604be3f5672a525997be16c289971df4a0
adalynnblackburn <adalynn.blackburn@ucas-edu.net>
2025-10-10 14:41:38 -0600

    did crap

--- c_programming_2/practices/name_decorator.c
+++ c_programming_2/practices/name_decorator.c
@@ -1,13 +1,8 @@
 // AB 7th Name Decorator
 
-#include <stdio.h>
-#include <string.h>
+int main(){
+    printf("What is your name: ");
+    scanf
 
-int main(void){
-    char name[25];
-    printf("Tell me your name: \n");
-    scanf("%s", name);
-
-    printf("****");
     return 0;
 }
