f10daf237fe1e38be2cc0178117ae47b4d40ef94
programming <adalynn.blackburn@ucas-edu.net>
2025-10-11 02:26:10 +0000

    finished name decorator

--- c_programming_2/practices/name_decorator.c
+++ c_programming_2/practices/name_decorator.c
@@ -1,8 +1,18 @@
 // AB 7th Name Decorator
 
-int main(){
-    printf("What is your name: ");
-    scanf
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[50];
+    char decorated[100];
+
+    printf("Enter your name: ");
+    scanf("%49s", name);
+
+    sprintf(decorated, "<<< %s >>>", name);
+
+    printf("%s\n", decorated);
 
     return 0;
 }
