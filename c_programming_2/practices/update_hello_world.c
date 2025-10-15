edf2b6a252cd67e38523a2ede99f8d5255bf71c6
adalynnblackburn <adalynn.blackburn@ucas-edu.net>
2025-10-15 14:13:48 -0600

    did update hello world

---
+++ c_programming_2/practices/update_hello_world.c
@@ -0,0 +1,17 @@
+// AB Update Hello World
+
+#include <stdio.h>
+
+void hello(char*name){
+    printf("Hello %s!\n", name);
+}
+
+int main(void){
+    hello("Andrew");
+    hello("Tia");
+    hello("Gary");
+    hello("Addy");
+    hello("Tim");
+
+return 0;
+}
